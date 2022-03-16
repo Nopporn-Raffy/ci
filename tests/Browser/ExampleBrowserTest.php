7a2a5f50f2e661d8fba7d346add7fe61f846b51e
Nopporn-Raffy <94434737+Nopporn-Raffy@users.noreply.github.com>
2022-03-16 10:31:25 +0700

    edit

---
+++ tests/Browser/ExampleBrowserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+
+class ExampleBrowserTest extends DuskTestCase
+{
+    public function testBasicExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+    }
+
+    public function testLink()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->clickLink('Click Me')->assertSee("Here is my response");
+        });
+    }
+}
